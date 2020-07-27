e4dcd5b48e7eb5133fac72dc04845ec07b104d1b
james <jtwebco@gmail.com>
2020-07-27 11:19:46 -0400

    init

---
+++ build/precache-manifest.1b1fe9b3100948487126edf68260986f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b23ede5e24e985c1c0f3fb72d97d88c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3d4fc8ef3b3b5d8a5f8",
+    "url": "/static/css/main.a0b562b7.chunk.css"
+  },
+  {
+    "revision": "6d82bb3bb0b1f4cd391e",
+    "url": "/static/js/2.186d92a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.186d92a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3d4fc8ef3b3b5d8a5f8",
+    "url": "/static/js/main.ce7cb54e.chunk.js"
+  },
+  {
+    "revision": "e73fcc637bd94cd0dbcc",
+    "url": "/static/js/runtime-main.6ac7ec44.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
