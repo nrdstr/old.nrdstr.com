38b3818bd4d271092a769716a7c8fea16f94ee19
james <jtwebco@gmail.com>
2020-09-22 18:14:27 -0400

    testing svg load time

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import { NrdstrIcon } from '../icons/icons'
 
 const Loader = () => {
 
     return (
         <div className="loader-container">
-            <div className="item item-1"></div>
-            <div className="item item-2"></div>
-            <div className="item item-3"></div>
-            <div className="item item-4"></div>
+            <div className="item item-1"><NrdstrIcon /></div>
+            <div className="item item-2"><NrdstrIcon /></div>
+            <div className="item item-3"><NrdstrIcon /></div>
+            <div className="item item-4"><NrdstrIcon /></div>
         </div>
     )
 }
